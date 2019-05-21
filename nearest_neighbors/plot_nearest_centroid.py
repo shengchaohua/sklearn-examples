474bebec7938934d9cd6dd7a441a281f9488ba40
shengchaohua <shengchaohua163@163.com>
2019-05-21 19:05:54 +0800

    add plot_nearest_centroid.py

---
+++ nearest_neighbors/plot_nearest_centroid.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import datasets
+from sklearn.neighbors import NearestCentroid
+
+n_neighbors = 15
+
+# Import some data to play with
+iris = datasets.load_iris()
+# Take the first two features
+X = iris.data[:, :2]
+y = iris.target
+
+h = .02 # step size in the mesh
+
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+plt.figure(figsize=(16, 6))
+for i, shrinkage in enumerate([None, .2]):
+    # Create an instance of Neighbours Classifier and fit the data.
+    clf = NearestCentroid(shrink_threshold=shrinkage)
+    clf.fit(X, y)
+    y_pred = clf.predict(X)
+    print(shrinkage, np.mean(y == y_pred))
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.subplot(1, 2, i + 1)
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.title("3-Class classification (shrink_threshold=%r)"
+              % shrinkage)
+    plt.axis('tight')
+
+plt.show()
